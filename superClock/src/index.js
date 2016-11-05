db39b415dc6f9356ba68a3bf015876740d050559
davidkatz <15Dkatz@shcp.edu>
2016-11-05 10:13:22 -0700

    WatchMaster Step 3

--- superClock/src/index.js
+++ superClock/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-  <div>WatchMaster</div>,
+  <App />,
   document.getElementById('root')
 )
