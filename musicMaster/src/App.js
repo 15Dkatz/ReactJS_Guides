bb88b82438d6932d36030ea3d45fa919001d11d7
davidkatz <15Dkatz@shcp.edu>
2016-11-06 09:53:56 -0800

    MusicMaster Step 4: Search on Enter key

--- musicMaster/src/App.js
+++ musicMaster/src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar';
+// import SearchBar from './SearchBar';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-title">
           MusicMaster
         </div>
-        <SearchBar />
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {event.key === 'Enter' ? this.search() : console.log(event.key)}}
+            />
+            <InputGroup.Addon
+              onClick={() => this.search()}
+
+            >
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
       </div>
     )
+  }
+
+
+  search() {
+    console.log('query', this.state.query);
   }
 }
 
