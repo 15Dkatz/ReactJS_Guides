c78df14f9730b821fcb2cfa247782ddbb6b0a1e8
davidkatz <15Dkatz@shcp.edu>
2016-11-06 10:11:31 -0800

    MM Step 5: Query Strings

--- musicMaster/src/App.js
+++ musicMaster/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
               placeholder="Search for an Artist"
               value={this.state.query}
               onChange={event => this.setState({query: event.target.value})}
-              onKeyPress={event => {event.key === 'Enter' ? this.search() : console.log(event.key)}}
+              onKeyPress={event => {event.key === 'Enter' ? this.search() : console.log()}}
             />
             <InputGroup.Addon
               onClick={() => this.search()}
@@ -41,6 +41,16 @@ class App extends Component {
 
   search() {
     console.log('query', this.state.query);
+
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      console.log('responseJSON', responseJSON);
+    })
   }
 }
 
