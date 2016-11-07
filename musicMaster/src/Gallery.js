5459d1dea0e8337d7a9c31ebdc465a0d533cc1c2
davidkatz <15Dkatz@shcp.edu>
2016-11-06 18:15:07 -0800

    MM Step 11: Add Audio object

--- musicMaster/src/Gallery.js
+++ musicMaster/src/Gallery.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false,
+      albumPlaying: ''
+    }
+  }
+
   render() {
     let tracks = this.props.tracks !== undefined ? this.props.tracks : [];
-
     return (
       <div className="Gallery">
         {
           tracks.map((track, k) => {
             let trackImg = track.album.images[0].url;
+            let previewUrl = track.preview_url;
             return (
-              <div key={k} className="track">
+              <div
+                key={k}
+                className="track"
+                onClick={() => this.audio(previewUrl)}
+              >
                 {/*{track.name}*/}
                 <img
                   src={trackImg}
@@ -26,6 +38,12 @@ class Gallery extends Component {
         }
       </div>
     )
+  }
+
+  audio(previewUrl) {
+    console.log('play', previewUrl);
+    let audio = new Audio(previewUrl); // native js object
+    audio.play();
   }
 }
 
