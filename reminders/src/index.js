01c51876379703e8cffbe6301217e7ee661b7ed3
davidkatz <15Dkatz@shcp.edu>
2016-12-13 10:12:56 -0800

    finish step 1: entry point

--- reminders/src/index.js
+++ reminders/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(
-  <App />,
+render(
+  <div>
+    reminders
+  </div>,
   document.getElementById('root')
-);
+)
